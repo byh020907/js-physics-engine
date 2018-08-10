258bb0291ab22efa0122a7a38c5c3fadfcedfae2
BAE Yong Ho <byh020907@naver.com>
2018-08-10 23:19:52 +0900

    touchevent추가중

--- public/scripts/Input.js
+++ public/scripts/Input.js
@@ -6,12 +6,47 @@ window.addEventListener("mousedown", mouseDown, false);
 window.addEventListener("mouseup", mouseUp, false);
 window.addEventListener("mousemove", mouseMove, false);
 
+window.addEventListener("touchstart", onTouch, false);
+window.addEventListener("touchmove", onTouch, false);
+window.addEventListener("touchend", onTouch, false);
+
+
 var keys = [];
 var currentKeys = [];
 var mouses = [];
 var currentMouses = [];
 var mousePos = new Vector2d(0, 0);
 var pMousePos = new Vector2d(0, 0);
+
+function onTouch(evt) {
+  evt.preventDefault();
+  if (evt.touches.length > 1 || (evt.type == "touchend" && evt.touches.length > 0))
+    return;
+
+  var newEvt = document.createEvent("MouseEvents");
+  var type = null;
+  var touch = null;
+
+  switch (evt.type) {
+    case "touchstart":
+      type = "mousedown";
+      touch = evt.changedTouches[0];
+      break;
+    case "touchmove":
+      type = "mousemove";
+      touch = evt.changedTouches[0];
+      break;
+    case "touchend":
+      type = "mouseup";
+      touch = evt.changedTouches[0];
+      break;
+  }
+
+  newEvt.initMouseEvent(type, true, true, evt.originalTarget.ownerDocument.defaultView, 0,
+    touch.screenX, touch.screenY, touch.clientX, touch.clientY,
+    evt.ctrlKey, evt.altKey, evt.shiftKey, evt.metaKey, 0, null);
+  evt.originalTarget.dispatchEvent(newEvt);
+}
 
 function isKeyDown(keyCode) {
     return currentKeys[keyCode];
